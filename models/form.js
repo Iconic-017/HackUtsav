cd5de1d546ccf2baa66c62cae2f3f054cec59cc2
= <=>
2024-07-04 13:42:29 +0530

    Updated database schema

--- models/form.js
+++ models/form.js
@@ -1,34 +1,53 @@
 const mongoose = require("mongoose");
 
 const schema = new mongoose.Schema({
-    name: {
-        type : String,
+    members: {
+        type: Number,
+        validate: {
+            validator: function(v) {
+                return v === 1 || v === 2;
+            },
+            message: 'Team members can be either 1 or 2'
+        },
+        required: true
+    },
+
+    teamName: {
+        type: String,
         required : true
     },
-    email: {
-        type: String,
-        required : true,
-        unique : true
-    },
-    contact : {
-        type: Number,
-        required : true,
-        unique : true
-    },
+
     college: {
         type : String , 
         required : true
     },
-    teamName: {
-        type: String,
+
+    name1: {
+        type : String,
         required : true
+    }, 
+    email1: {
+        type: String,
+        required : true,
+        unique : true
+    },
+    contact1 : {
+        type: Number,
+        required : true,
+        unique : true,
+        default : none
     },
     name2 : {
         type : String,
         default : "none"
     },
+    email2 : {
+        type : String,
+        default : "none"
+    },
     contact2 : {
         type : Number,
+        default : "none"
     },
     upiPaymentId: {
         type: String,
